9403b1d87832f240f43575ad9775f4eff7418ffb
franco <frantejes25@gmail.com>
2026-09-16 00:41:56 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/TP2---Lenguajes-IV---Franco-Tiago-Tejerina-Frola/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
